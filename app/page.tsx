d2fb3609a89d95a8d84032f56cfe02a67cb4e445
MahdMalik <mahdmahdmalik@gmail.com>
2025-04-19 16:39:30 -0500

    I LOVE INHALING SODDER PASTE GRAHHHH

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 
 export default function Home() {
   const [userData, setUserData] = useState([])
+  const [graph, setGraph] = useState("")
   
   //function that calls python
   const callPython = async() => {
@@ -17,6 +18,7 @@ export default function Home() {
     if(data.success == true)
     {
       console.log(data.message)
+      setGraph(`data:image/png;base64,${data.image}`)
     }
     else
     {
@@ -28,6 +30,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <p>Click the button if you're goated:</p>
       <Button onClick={callPython}>Python generator</Button>
+      <img src={graph}></img>
     </div>
   );
 }
