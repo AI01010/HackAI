52429f29caf93be731f944ed127f13c2ce6199a5
MahdMalik <mahdmahdmalik@gmail.com>
2025-04-19 15:14:00 -0500

    added mmui

--- .history/app/page_20250419151102.tsx
+++ .history/app/page_20250419151102.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
-import Button
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p>Click the button if you're goated:</p>
-      <Button>Python generator</Button>
+      </footer>
     </div>
   );
 }
