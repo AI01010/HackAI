28661ca21c268bad4a39925ce37bf7bc0df2ae86
MahdMalik <mahdmahdmalik@gmail.com>
2025-04-20 10:45:16 -0500

    PLEASE WORK FIRST TRY RAHHHHH

--- Backend/GetData.py
+++ Backend/GetData.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 CORS(app)  # Allow cross-origin requests
 
 # Load the trained neural network model
-foodModel = tf.keras.models.load_model('MlAlgos/neural_network_model.keras')
+foodModel = tf.keras.models.load_model('MlAlgos/food_neural_network_model.keras')
 
 @app.route('/python/getResults', methods=["POST"])
 def getResults():
@@ -41,7 +41,6 @@ def getResults():
 def GetFoods():
     data = request.get_json()
     year = data["year"]
-    print("Year: " + str(year))
     min = data["leftRange"]
     max = data["rightRange"]
 
@@ -100,6 +99,53 @@ def GetFoods():
     finalStr = base64.b64encode(imgBuffer.read()).decode("utf-8")
 
     return jsonify({"success": True, "message": "GRAHHHH", "image": finalStr, "numFoods": len(foodArr)})
+
+@app.route("/python/GetMonthlySales", methods=["POST"])
+def GetMonthlySales():
+    data = request.get_json()
+    year = data["year"]
+    monthNames = ["August, February, March, April, May, June, July, August, September"]
+    monthSales = list()
+    monthData = pd.read_csv("FinalData/SalesPerMonth.csv")
+
+    scaler = StandardScaler()
+    scaler.fit(monthData[["Date"]])  # Use 'Date' column for scaling
+
+    # for each of the 9 months
+    for i in range(9):
+        sample_input = pd.DataFrame({
+            "Month": [i + 1],
+            "Year": [year]  # Replace `year` with the actual year value for prediction
+        })
+
+        # Preprocess the input data (encode 'Food' and scale 'Date')
+        sample_input['Month'] = scaler.transform(sample_input[['Month']])  # Scale 'Date'
+        sample_input[['Year']] = scaler.transform(sample_input[['Year']])  # Scale 'Date'
+
+        # Make a prediction with the neural network
+        estCost = foodModel.predict(sample_input)
+
+        # Extract the predicted revenue (assuming it's a scalar output)
+        estCost = estCost[0][0]  # Extract the scalar value from the 2D array
+        monthSales.append(estCost)
+    
+    plt.bar(monthNames, monthSales)
+    plt.xlabel("Month")
+    plt.xticks(fontsize=6)
+    plt.ylabel("Sales")
+    plt.title("Revenue in Months For Year")
+
+    # Save the plot to a buffer
+    imgBuffer = io.BytesIO()
+    plt.savefig(imgBuffer, format="png")
+    plt.close()
+
+    imgBuffer.seek(0)
+
+    # Convert the image to a base64 string
+    finalStr = base64.b64encode(imgBuffer.read()).decode("utf-8")
+
+    return jsonify({"success": True, "message": "GRAHHHH", "image": finalStr})
 
 if __name__ == '__main__':
     print("app is running")
